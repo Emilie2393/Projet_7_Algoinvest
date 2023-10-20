abfbf65e1bca99b32f1e0e8bb3c567e0baa8b7cf
Emily2393 <emilie.rich4rd@gmail.com>
2023-10-20 13:49:18 +0200

    optimized bug fix

--- optimized.py
+++ optimized.py
@@ -1,4 +1,4 @@
-max_cost = 500
+max_cost = 100
 actions = [[20, 5], [30, 10], [50, 15], [70, 20], [60, 17], [80, 25],
            [22, 7], [26, 11], [48, 13], [34, 27], [42, 17],
            [110, 9], [38, 23], [14, 1], [18, 3], [8, 8], [4, 12],
@@ -23,13 +23,20 @@ selection = []
 
 while max_cost >= 0 and indice >= 0:
     e = actions[indice-1]
-    print(e)
-    print(table[indice][max_cost])
-    print(table[indice-1][max_cost-e[0]] + (e[0]*e[1]/100))
-    if table[indice][max_cost] == round(table[indice-1][max_cost-e[0]] + (e[0]*e[1]/100), 2):
-        selection.append(e)
-        max_cost -= e[0]
-    indice-=1
+    print(e, indice, max_cost)
+    print("actuel", table[indice][max_cost])
+    print("= ligne pre",table[indice-1])
+    print("indice pre", [max_cost-e[0]])
+    if abs(max_cost-e[0]) > len(actions) and abs(max_cost-e[0]) > len(table[0]):
+        break
+    else:
+        #compare derniere cellule de la ligne de la derniere action (meilleur resultat), si celle ci correspond 
+        #à la (ligne precedente - le cout de cette action) + son benefice, cela veut dire qu'elle est responsable
+        #du meilleur benefice de la ligne table[indice][max_cost]
+        if table[indice][max_cost] == round(table[indice-1][max_cost-e[0]] + (e[0]*e[1]/100), 2):
+            selection.append(e)
+            max_cost -= e[0]
+        indice-=1
 
 print(table[-1][-1], selection)
     
