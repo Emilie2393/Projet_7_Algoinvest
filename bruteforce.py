c501ddd451d6828dd6aa3f12c06c3a1e47523723
Emily2393 <emilie.rich4rd@gmail.com>
2023-11-02 17:21:19 +0100

    files optimisation

--- bruteforce.py
+++ bruteforce.py
@@ -1,43 +1,50 @@
 from itertools import combinations
 import time
+import sys
 
-class ForceBrute:
-    def __init__(self, data, max_cost):
-        self.data = data
-        self.max_cost = max_cost
-     
-    def calculate(self):
+actions = [[1, 20, 5], [2, 30, 10], [3, 50, 15], [4, 70, 20], [5, 60, 17], [6, 80, 25],
+        [7, 22, 7], [8, 26, 11], [9, 48, 13], [10, 34, 27], [11, 42, 17],
+        [12, 110, 9], [13, 38, 23], [14, 14, 1], [15, 18, 3], [16, 8, 8], [17, 4, 12],
+        [18, 10, 14], [19, 24, 21], [20, 114, 18]]
+max_cost = 500
+for action in actions:
+    action[2] = action[1] * action[2] / 100
+
+current_cost = 0
+top_gain = 0
+top_combinaison = []
+current_profit = []
+top_cost = 0
+before = sys.getallocatedblocks()
+start = time.time()
+
+for i in range(len(actions)):
+    # combination of i * number of action in actions list 
+    comb = combinations(actions, i)
+    # for each comparation 
+    for combinaison in list(comb):
+        # for each action in each combination 
+        for action in combinaison:
+            # if action cost is not superior of max_cost with
+            # cumulate action's cost in current_cost
+            if action[1] <= max_cost-current_cost:
+                # cumulate action cost to previous actions cost
+                current_cost += action[1]
+                # add action profit to previous profit list
+                current_profit.append(action[2])
+            else:
+                break
+        # compare actual profit list with previous best gain
+        if sum(current_profit) > top_gain:
+            top_gain = sum(current_profit)
+            top_combinaison = combinaison
+            top_cost = current_cost
+        # init variables for next combination 
         current_cost = 0
-        top_gain = 0
-        top_combinaison = []
         current_profit = []
 
-        start = time.time()
-
-        for i in range(len(self.data)):
-            # combination of i * number of action in actions list 
-            comb = combinations(self.data, i)
-            # for each comparation 
-            for combinaison in list(comb):
-                # for each action in each combination 
-                for action in combinaison:
-                    # while maximum cost of each action is inferior at 500
-                    # and action cost is not superior of current current_cost
-                    if current_cost <= self.max_cost and action[0] <= self.max_cost-current_cost:
-                        # cumulate action cost to previous actions cost
-                        current_cost += action[0]
-                        # add action profit to previous profit list
-                        current_profit.append(action[0] * action[1] / 100)
-                    else:
-                        break
-                # compare actual profit list with previous best gain
-                if sum(current_profit) > top_gain:
-                    top_gain = sum(current_profit)
-                    top_combinaison = combinaison
-                # init variables for next combination 
-                current_cost = 0
-                current_profit = []
-
-        print("Brut force - La meilleure rentabilité est: ", round(top_gain, 2)," pour les actions suivantes: \n", top_combinaison)
-        end = time.time()
-        print(end - start)
+print("Brut force - La meilleure rentabilité est:", round(top_gain, 2),"€ pour un cout de", top_cost, "€ pour les actions suivantes: \n", top_combinaison)
+end = time.time()
+print("Le temps d'execution est de", round(end - start, 3), "secondes")
+diff = sys.getallocatedblocks() - before
+print(diff, "blocs mémoires supplémentaires ajoutés")
--- optimized.py
+++ optimized.py
@@ -1,47 +1,55 @@
 import time
+import csv
+import sys
 
-class Knapsack():
-    def __init__(self, data, max_cost):
-        self.data = data
-        self.max_cost = max_cost
-    
-    def calculate(self):
-        start = time.time()
-        # create a table with 501 columns (max_cost) and 21 rows (actions length)
-        table = [[0 for i in range(self.max_cost + 1)] for i in range(len(self.data) + 1)]
+actions = []
+file = open("dataset2_Python+P7.csv")
+reader = csv.reader(file)
+next(reader)
+for row in reader:
+    if float(row[1]) > 0:
+        row[1], row[2] = int(float(row[1]) * 100), round((float(row[1]) * float(row[2]) / 100),3)
+        actions.append(row)
 
-        for action in range(1, len(self.data) + 1):
-            for cost in range(1, self.max_cost + 1):
-                # if action's cost is less than current cost range
-                if self.data[action-1][0] <= cost:
-                    # set best result between current action and previous action results for the same cost limitation 
-                    table[action][cost] = max(round((self.data[action-1][0] * self.data[action-1][1]/100) + 
-                                                     table[action-1][cost-self.data[action-1][0]], 2), table[action-1][cost])
-                else:
-                    # set previous action profit
-                    table[action][cost] = table[action-1][cost]
+max_cost = 50000
+before = sys.getallocatedblocks()
+start = time.time()
+# create a table with 501 columns (max_cost) and 21 rows (actions length)
+table = [[0 for i in range(max_cost + 1)] for i in range(len(actions) + 1)]
 
-        indice = len(self.data)
-        selection = []
+for action in range(1, len(actions) + 1):
+    for cost in range(1, max_cost + 1):
+        # if action's cost is less than current cost range
+        if actions[action-1][1] <= cost:
+            # set best result between current action and previous action results for the same cost limitation 
+            table[action][cost] = max(round(actions[action-1][2] + table[action-1][cost-actions[action-1][1]], 2), 
+                                        table[action-1][cost])
+        else:
+            # set previous action profit
+            table[action][cost] = table[action-1][cost]
 
-        while self.max_cost >= 0 and indice >= 0:
-            e = self.data[indice-1]
-            # if absolute result of self.max_cost - previous action cost is more than self.data and table length
-            # prevent an error which occurs when self.max_cost is lower than current action cost
-            if abs(self.max_cost-e[0]) > len(self.data) and abs(self.max_cost-e[0]) > len(table[0]):
-                indice-=1
-            else:
-                # compare best result of current action with previous action results - current action cost + his profit,
-                # if results fits, it meens that this action is responsible for the best result 
-                # table[indice][self.max_cost] : current action will be saved
-                if table[indice][self.max_cost] == round(table[indice-1][self.max_cost-e[0]] + (e[0]*e[1]/100), 2):
-                    selection.append(e)
-                    self.max_cost -= e[0]
-                indice-=1
+indice = len(actions)
+selection = []
+total_cost = []
+while max_cost >= 0 and indice >= 0:
+    e = actions[indice-1]
+    # if absolute result of max_cost - previous action cost is more than actions and table length
+    # prevent an error which occurs when max_cost is lower than current action cost
+    if abs(max_cost-e[1]) > len(actions) and abs(max_cost-e[1]) > len(table[0]):
+        indice-=1
+    else:
+        # compare best result of current action with previous action results - current action cost + his profit,
+        # if results fits, it meens that this action is responsible for the best result 
+        # table[indice][max_cost] : current action will be saved
+        if table[indice][max_cost] == round(table[indice-1][max_cost-e[1]] + e[2], 2):
+            selection.append(e[0])
+            max_cost -= e[1]
+            total_cost.append(e[1])
+        indice-=1
 
-        print("Knapsack - La meilleure rentabilité est:", table[-1][-1]," pour les actions suivantes: \n", selection)
-
-        end = time.time()
-        print(end - start)
-    
+print("Knapsack - La meilleure rentabilité est:", table[-1][-1],"€ pour un cout de", sum(total_cost)/100, "€ pour les actions suivantes: \n", selection)
+end = time.time()
+print("Le temps d'execution est de", round(end - start, 3), "secondes")
+diff = sys.getallocatedblocks() - before
+print(diff, "blocs mémoires supplémentaires ajoutés")
 
